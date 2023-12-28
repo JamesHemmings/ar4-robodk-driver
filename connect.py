a421cd49ec9a6d97f07f8fd1923cec277a2cf811
james <james.richard.hemmings@gmail.com>
2023-12-27 22:27:38 -0800

    Refactor client code to separate connection and message sending functionality

--- connect.py
+++ connect.py
@@ -2,32 +2,31 @@ import socket
 from text_2_speech import text_to_speech
 from voice_2_text import recognize_speech_from_mic
 
+test_string = "LLA1B0C0D0E0F0G0H0I0" + "J0K-3.96L0M1.334N30.005O10.5P0Q0R0" + "\n"
 
-def client_program():
-    host = "raspberrypi.local"  # Replace with Raspberry Pi's IP address
+def connect_to_server():
+    host = "raspberrypi.local"  # Raspberry Pi's IP address
     port = 5000  # The same port as used by the server
 
     client_socket = socket.socket()
     client_socket.connect((host, port))
-    text_to_speech(f"client connected to{host}")
+    text_to_speech(f"client connected to {host}")
 
-    message = input(" -> ")
+    return client_socket
 
-    while message.lower().strip() != 'bye':
-        text_to_speech("Please say a command")
-        client_socket.send(message.encode())
-        data = client_socket.recv(1024).decode()
-        print('Received from server: ' + data)
-        message = recognize_speech_from_mic()
-
-    client_socket.close()
-
+def send_message(client_socket, message):
+    client_socket.send(message.encode())
+    data = client_socket.recv(1024).decode()
+    print('Received from server: ' + data)
+    return data
 
 if __name__ == '__main__':
-    client_program()
+    client_socket = connect_to_server()
+    message = input(" -> ")
+    message = test_string
 
+    while message.lower().strip() != 'bye':
+        response = send_message(client_socket, message)
+        message = input(" -> ")
 
-
-
-
-
+    client_socket.close()
